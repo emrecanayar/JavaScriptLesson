f3bb03e6bbe0b6fc168f768339eb35e00352396f
Emre Can Ayar <emrecan.ayar@hotmail.com>
2022-08-02 20:13:00 +0300

    Promises Aysnc-Await

---
+++ 19_Promises/3/app.js
@@ -0,0 +1,35 @@
+import fetch from 'node-fetch'
+
+const BASE_URL = "https://jsonplaceholder.typicode.com";
+
+// const fetchUsers = async () => {
+
+//     const response = await fetch(`${BASE_URL}/users`);
+//     const usersData = await response.json();
+
+//     return usersData;
+// }
+
+
+// const users = await fetchUsers();
+
+// console.log(users);
+
+
+const fetchUsers = async () => {
+
+    try {
+        const response = await fetch(`${BASE_URL}/users`);
+        const usersData = await response.json();
+        return usersData;
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+}
+
+
+const users = await fetchUsers();
+
+console.log(users);
+
